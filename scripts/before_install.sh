f2142a6f86f88d0542e85d37dc9acda46a869642
Mridul <59385288+Mrid01@users.noreply.github.com>
2023-07-29 19:08:02 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ubuntu/server
+cd /home/ec2-user/server
 curl -sL https://deb.nodesource.com/setup_18.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt-get install -y nodejs
