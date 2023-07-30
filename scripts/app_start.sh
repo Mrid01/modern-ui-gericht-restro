399e04144b708e4ca0e3fbbdf94606a2ca70d712
Mridul <59385288+Mrid01@users.noreply.github.com>
2023-07-30 12:08:38 +0530

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /home/ec2-user/server/src
+cd /home/ec2-user/server/
 npm start
-pm2 start npm --name "gericht" -- start
 pm2 startup
+pm2 serve build 8082 --spa
 pm2 save
 pm2 restart all
